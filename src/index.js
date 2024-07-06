c668c1a3afad523517ba6508bda12205dcfb44e6
severitech <padilladouglas6@gmail.com>
2024-07-06 00:19:35 -0400

    Primer componente creado

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 //Importar React
-import React from 'react'
+import React from "react";
 //Utilizar el dom con react
-import ReactDom from 'react-dom/client'
+import ReactDom from "react-dom/client";
 
 //Usar el dom para añadir un elemento al componente div con el id root
-const root = ReactDom.createRoot(document.getElementById('root'))
+const root = ReactDom.createRoot(document.getElementById("root"));
 //enviamos a esa constantes elementos html
-root.render(<h1> Hola mundo</h1>)
+
+//este es un componente
+function Greeting() {
+  const user = {
+    firstname: "Douglas",
+    lastname: "Severitech",
+  };
+
+  return ( <><h1>{user.firstname}</h1>
+<h3>{user.lastname}</h3></>)
+
+  
+}
+
+root.render(<><Greeting /></>);
